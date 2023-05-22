acf9fe7f5dc0d8bea37685d48c50a55d9bfe95f7
LingGrani <luthfiangger2510@gmail.com>
2023-05-22 11:29:34 +0700

    Membuat Class Orang

--- ParadigmaOOP_137/ParadigmaOOP_137.cpp
+++ ParadigmaOOP_137/ParadigmaOOP_137.cpp
@@ -1,20 +1,19 @@
-// ParadigmaOOP_137.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include <string>
+using namespace std;
 
-int main()
+class orang
 {
-    std::cout << "Hello World!\n";
-}
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+public:
+    string nama;
+    orang(string pNama) :
+        nama(pNama) {
+        cout << "Orang dibuat\n" << endl;
+    }
+    ~orang() {
+        cout << "Orang dihapus\n" << endl;
+    };
+    int jumlah(int a, int b) {
+        return a + b;
+    }
+};
