5c04bfad08e19940d22af176c626a1c903522999
LingGrani <luthfiangger2510@gmail.com>
2023-05-22 19:00:07 +0700

    Membuat class dokter dan membernya

--- Asosiasi/Asosiasi.cpp
+++ Asosiasi/Asosiasi.cpp
@@ -12,8 +12,23 @@ public:
 		cout << "Pasien \"" << nama << "\" ada\n";
 	}
 	~pasien() {
-		cout << "pasien\"" << nama << "\" tidak ada\n";
+		cout << "Pasien\"" << nama << "\" tidak ada\n";
 	}
 	void tambahDokter(dokter*);
 	void cetakDokter();
+};
+
+class dokter
+{
+public:
+	string nama;
+	vector<pasien*> daftar_pasien;
+	dokter(string pNama) :nama(pNama) {
+		cout << "Dokter \"" << nama << "\" ada\n";
+	}
+	~dokter() {
+		cout << "Dokter\"" << nama << "\" tidak ada\n";
+	}
+	void tambahPasien(pasien*);
+	void cetakPasien();
 };
